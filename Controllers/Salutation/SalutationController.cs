4fc9503e689eb6c85217aae86628d54fa7332cb8
Ablayekandji <kandji.djibrilabdoulaye@gmail.com>
2022-11-08 13:11:53 +0200

    second commit

---
+++ Controllers/Salutation/SalutationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers.Salutation
+{
+    public class SalutationController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            Visiteur client = new Visiteur();
+            return View(client);
+           
+        }
+
+        [HttpPost]
+        public ActionResult Index(Visiteur visiteur)
+        {
+            Visiteur client = new Visiteur();
+            string prenom = "";
+
+            prenom = Request.Form["prenom_visiteur"];
+            client.Prenom = prenom;
+            ViewData["message"] = "Bonjour à toi, " + prenom;
+            return View("Index", client);
+        }
+
+        /*
+            Projet présentation de l’UFR SETs
+            Vous aurez à créer une base de données ; 
+            •	L’application web mvc va présenter l’UFR surtout
+            •	La liste des  départements
+            •	La liste des filières
+            •	Les PERs
+            •	L’administration
+            conditions d'acces
+         */
+    }
+}
